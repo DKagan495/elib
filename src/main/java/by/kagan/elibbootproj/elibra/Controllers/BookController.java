1dfdde4a9d7e1961b60f72252f4344dbb15fe259
Daniil Kagan <dan.kagan@yandex.by>
2021-07-06 16:10:57 +0300

    added show all books feature + 1 dao(book) + 1 template(thymeleaf)(show all books page)

--- src/main/java/by/kagan/elibbootproj/elibra/Controllers/BookController.java
+++ src/main/java/by/kagan/elibbootproj/elibra/Controllers/BookController.java
@@ -3,8 +3,11 @@ package by.kagan.elibbootproj.elibra.Controllers;
 import by.kagan.elibbootproj.elibra.DAO.BookDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 @Controller
 public class BookController {
@@ -15,5 +18,9 @@ public class BookController {
         this.bookDAO = bookDAO;
         this.httpSession = httpSession;
     }
-
+    @GetMapping("/booklist")
+    public String showBookList(Model model) throws SQLException {
+        model.addAttribute("booklist", bookDAO.showAll());
+        return "booklist";
+    }
 }
